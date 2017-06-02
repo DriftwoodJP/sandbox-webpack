a851c155c22e4f016c26bec2de5a1ecfc3f78d75
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-06-02 14:35:35 +0900

    Add: babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,16 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
