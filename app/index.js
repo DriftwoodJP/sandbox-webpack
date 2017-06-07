f38833e5202e81ec81ddfb3f3b768ff1779581b6
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2017-06-07 10:57:28 +0900

    Fix: sample css code

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 function component() {
   let element = `
-    <div class="your-class">
-      <div>your content 1</div>
-      <div>your content 2</div>
-      <div>your content 3</div>
-    </div>
+    <ul class="slide">
+      <li><img src="https://placeimg.com/640/240/animals"></li>
+      <li><img src="https://placeimg.com/640/240/arch"></li>
+      <li><img src="https://placeimg.com/640/240/nature"></li>
+    </ul>
   `;
 
   return element;
@@ -12,10 +12,13 @@ function component() {
 
 document.write(component());
 
+
+// slick-carousel
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-$('.your-class').slick();
-
 import styles from './stylesheets/style.css';
+
+$(function() {
+  $('.slide').slick();
+});
